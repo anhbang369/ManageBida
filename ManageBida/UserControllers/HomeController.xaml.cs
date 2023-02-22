acb2d16b1c8634fbd950dee2870d569988101ac1
anhbang369 <107161738+anhbang369@users.noreply.github.com>
2023-02-22 10:51:27 +0700

    bang

---
+++ ManageBida/UserControllers/HomeController.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManageBida.UserControllers
+{
+    /// <summary>
+    /// Interaction logic for HomeController.xaml
+    /// </summary>
+    public partial class HomeController : UserControl
+    {
+        public HomeController()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(new TotalController());
+
+        }
+
+        private void btnTong_Click(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(new TotalController());
+        }
+
+        private void btnNote_Click(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(new NoteController());
+        }
+    }
+}
